0495d86f03378ff6106a86fd60473fe70701a722
coolsenma <anotherpony@163.com>
2020-05-22 16:49:37 +0800

    add springboot actuator config and database actuator demo

---
+++ src/main/java/com/nowcoder/community/actuator/DatabaseEndpoint.java
@@ -0,0 +1,43 @@
+package com.nowcoder.community.actuator;
+
+import com.nowcoder.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author coolsen
+ * @version 1.0.0
+ * @ClassName DatabaseEndpoint.java
+ * @Description Database Endpoint for actuator
+ * @createTime 2020/5/22 16:37
+ */
+
+@Component
+@Endpoint(id = "database")
+public class DatabaseEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseEndpoint.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    @ReadOperation
+    public String checkConnection(){
+        try (
+                Connection conn = dataSource.getConnection();
+        ) {
+            return CommunityUtil.getJSONString(0, "获取连接成功!");
+        } catch (SQLException e) {
+            logger.error("获取连接失败:" + e.getMessage());
+            return CommunityUtil.getJSONString(1, "获取连接失败!");
+        }
+    }
+}
--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                 )
                 .antMatchers(
                         "/discuss/delete",
-                        "/data/**"
+                        "/data/**",
+                        "/actuator/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN
