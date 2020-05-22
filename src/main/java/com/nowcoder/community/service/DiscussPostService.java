87fff6a14a330038faa90798119892e77614defa
coolsenma <anotherpony@163.com>
2020-05-22 15:46:19 +0800

    add caffeine to optimize top posts ranking

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -1,16 +1,28 @@
 package com.nowcoder.community.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.util.SensitiveFilter;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
@@ -18,11 +30,79 @@ public class DiscussPostService {
     @Autowired
     private SensitiveFilter sensitiveFilter;
 
-    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit,int orderMode) {
-        return discussPostMapper.selectDiscussPosts(userId, offset, limit,orderMode);
+    @Value("${caffeine.posts.max-size}")
+    private int maxSize;
+
+    @Value("${caffeine.posts.expire-seconds}")
+    private int expireSeconds;
+
+    // Caffeine核心接口: Cache, LoadingCache, AsyncLoadingCache
+
+    // 帖子列表缓存
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+
+    // 帖子总数缓存
+    private LoadingCache<Integer, Integer> postRowsCache;
+
+    @PostConstruct
+    public void init() {
+// 初始化帖子列表缓存
+        postListCache=Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Nullable
+                    @Override
+                    public List<DiscussPost> load(@NonNull String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("参数错误!");
+                        }
+                        String[] params = key.split(":");
+                        if (params == null || params.length != 2) {
+                            throw new IllegalArgumentException("参数错误!");
+                        }
+                        int offser = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+
+                        // TODO:二级缓存：redis->mysql
+
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0, offser, limit, 1);
+                    }
+                });
+
+        // 初始化帖子总数缓存
+        postRowsCache=Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Nullable
+                    @Override
+                    public Integer load(@NonNull Integer key) throws Exception {
+
+                        // TODO:二级缓存：redis->mysql
+
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
+
+    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode) {
+        // 只有热门帖子(访问首页时，userId=0)
+        if (userId == 0 && orderMode == 1) {
+            return postListCache.get(offset + ":" + limit);
+        }
+        logger.debug("load post list from DB.");
+        return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);
     }
 
     public int findDiscussPostRows(int userId) {
+        // 首页查询走缓存
+        if (userId == 0) {
+            return postRowsCache.get(userId);
+        }
+        logger.debug("load post list from DB.");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,49 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService postService;
+
+    // 自造数据，进行压力测试
+    @Test
+    public void initDataForTest() {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 300000; i++) {
+            DiscussPost post = new DiscussPost();
+            post.setUserId(111);
+            post.setTitle("互联网求职暖春计划");
+            post.setContent("今年的就业形势，确实不容乐观。过了个年，仿佛跳水一般，整个讨论区哀鸿遍野！19届真的没人要了吗？！18届被优化真的没有出路了吗？！大家的“哀嚎”与“悲惨遭遇”牵动了每日潜伏于讨论区的牛客小哥哥小姐姐们的心，于是牛客决定：是时候为大家做点什么了！为了帮助大家度过“寒冬”，牛客网特别联合60+家企业，开启互联网求职暖春计划，面向18届&19届，拯救0 offer！");
+            post.setCreateTime(new Date());
+            post.setScore(Math.random() * 2000);
+            postService.addDiscussPost(post);
+        }
+        long endTime = System.currentTimeMillis();
+        long totalTime=endTime-startTime;
+        System.out.println(String.format("总时间%dmin",totalTime/(1000*60)));//39min
+    }
+
+    @Test
+    public void testCache() {
+
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 0));
+    }
+
+}
